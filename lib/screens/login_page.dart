5abcde664bdf0856bf3958a1e50bccc9eb82637e
ETNinja <annealmd@gmail.com>
2020-08-05 19:02:26 -0300

    ButtonBar
    Button;bar
    Container size with mediaQuery conditional to the platform - > not working for flutter web, just for mobile

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -14,24 +15,54 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            SizedBox(height: 80.0),
-            Column(
-              children: [
-                Image.asset('assets/diamond.png'),
-                SizedBox(
-                  height: 16.0,
+        child: Center(
+          child: Container(
+            width: defaultTargetPlatform == TargetPlatform.android ||
+                    defaultTargetPlatform == TargetPlatform.iOS
+                ? MediaQuery.of(context).size.width * 1
+                : MediaQuery.of(context).size.width *
+                    0.60, // does not work on web browser
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              children: <Widget>[
+                SizedBox(height: 80.0),
+                Column(
+                  children: [
+                    Image.asset('assets/diamond.png'),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text('SPARKLE'),
+                  ],
                 ),
-                Text('SPARKLE'),
+                SizedBox(
+                  height: 120.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'UserName',
+                  ),
+                ),
+                SizedBox(
+                  height: 12.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                ButtonBar(
+                  children: [
+                    FlatButton(onPressed: null, child: const Text('Cancel')),
+                    RaisedButton(onPressed: null, child: const Text('NEXT')),
+                  ],
+                ),
               ],
             ),
-            SizedBox(
-              height: 120.0,
-              //TODO: username e passwords
-            ),
-          ],
+          ),
         ),
       ),
     );
