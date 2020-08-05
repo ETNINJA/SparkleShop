26d91aa34661199ee1a45e6bd9f1465d08908ad4
ETNinja <annealmd@gmail.com>
2020-08-05 14:26:48 -0300

    Logo
    Add logo and frame to work in

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,12 +8,30 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //TODO: text additing controllers
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: [
+                Image.asset('assets/diamond.png'),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text('SPARKLE'),
+              ],
+            ),
+            SizedBox(
+              height: 120.0,
+              //TODO: username e passwords
+            ),
+          ],
         ),
       ),
     );
