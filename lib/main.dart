e07b04a3c9cfc31e2fbb43f90b92958ee1d85e9c
ETNinja <annealmd@gmail.com>
2020-08-05 20:32:42 -0300

    split main
    Split main = main + app

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'screens/login_page.dart';
+import 'sparkleApp.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  visualDensity: VisualDensity.adaptivePlatformDensity,
-  primaryColor: Color(0xFFE7AEB9),
-  primaryColorLight: Color(0xFFFFDFE8),
-  primaryColorDark: Color(0xFFC871A8),
-  accentColor: Color(0xFF7EAFAB),
-  dividerColor: Color(0x1f6D42CE),
-);
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sparkle',
-      theme: lightTheme,
-      home: LoginPage(),
-    );
-  }
+  runApp(SparkleApp());
 }
---
+++ lib/sparkleApp.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:Sparkle/screen/home.dart';
+import 'screen/home.dart';
+import 'package:Sparkle/screen/login_page.dart';
+
+ThemeData lightTheme = ThemeData(
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  primaryColor: Color(0xFFE7AEB9),
+  primaryColorLight: Color(0xFFFFDFE8),
+  primaryColorDark: Color(0xFFC871A8),
+  accentColor: Color(0xFF7EAFAB),
+  dividerColor: Color(0x1f6D42CE),
+);
+
+class SparkleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sparkle',
+      theme: lightTheme,
+      home: HomePage(),
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+    );
+  }
+}
+
+Route<dynamic> _getRoute(RouteSettings settings) {
+  if (settings.name != '/login') {
+    return null;
+  }
+
+  return MaterialPageRoute<void>(
+    settings: settings,
+    builder: (BuildContext context) => LoginPage(),
+    fullscreenDialog: true,
+  );
+}
