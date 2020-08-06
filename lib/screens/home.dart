5a2b6e3d7d94d9171451a868683a7aeda46b0851
ETNinja <annealmd@gmail.com>
2020-08-06 12:17:08 -0300

    AppBar
    leading & trailling buttons

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,9 +4,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () => print('menu button'),
+        ),
+        title: const Text('Sparkle'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () => print('search button'),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () => print('filter button'),
+          ),
+        ],
+      ),
       body: Center(
         child: const Text('Welcome to Sparkle'),
       ),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
